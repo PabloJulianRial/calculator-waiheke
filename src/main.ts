0d4df8b6d667abbcc85198048a8f101a81f452e6
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-03-07 15:49:45 +0000

    add classes and selectors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import './style.scss'
+import "./style.scss";
+
+// window.onload = function () {
+//   document.getElementById("input")?.focus();
+// };
+const numbers = document.querySelectorAll<HTMLButtonElement>(".button__number-button");
+const display = document.querySelector<HTMLInputElement>(".display__text")
+const clear = document.querySelector<HTMLButtonElement>(".button__clear")
+const clearEntry = document.querySelector<HTMLButtonElement>(".button__clearEntry")
+const plus = document.querySelector<HTMLButtonElement>(".button__plus")
+const minus = document.querySelector<HTMLButtonElement>(".button__minus")
+const divide = document.querySelector<HTMLButtonElement>(".button__divide")
+const times = document.querySelector<HTMLButtonElement>(".button__times")
+const equals = document.querySelector<HTMLButtonElement>(".button__equals")
 
 
 
